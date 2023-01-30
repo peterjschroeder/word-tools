509e98a9caced9235e97e60df06c94b14088cc02
peter <peterjschroeder@gmail.com>
2023-01-30 06:55:12 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+        name='word-tools', 
+        version='0.1', 
+        description='Word tools', 
+        author='Peter J. Schroeder', 
+        author_email='peterjschroeder@gmail.com', 
+        url='https://github.com/peterjschroeder/world-tools',
+        scripts=['rhyme', 'synonym'],
+        install_requires=[]
+)
+
